b0890affc6304deec9215305a58c5ec055db6bba
Petro Podrezo <petro@podrezo.com>
2024-02-21 21:40:24 -0500

    Add logging to output received event

--- translate.rb
+++ translate.rb
@@ -6,6 +6,8 @@ require_relative './lib/telegram'
 def handle(event:, context:)
   config = YAML.load_file('./config.yml').freeze
 
+  puts 'Received event:', event.to_json
+
   openai_service = OpenAI.new(config['openai_api_key'])
   telegram_service = Telegram.new(config['telegram_bot_key'])
   webhook = TelegramWebhook.new(event, config['listen_string'], telegram_service, openai_service)
